d62b84fb2a15b7b556d95c6f50dbfc89f82ceae6
JegernOUTT <sergey.vakhreev@gmail.com>
2023-01-04 01:53:07 +1030

    popupLoginMessageOnce if is not logged in

--- src/main/kotlin/com/smallcloud/codify/account/LoginStateService.kt
+++ src/main/kotlin/com/smallcloud/codify/account/LoginStateService.kt
@@ -1,16 +1,16 @@
 package com.smallcloud.codify.account
 
 import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.project.ProjectManager
 import com.intellij.util.concurrency.AppExecutorUtil
+import com.smallcloud.codify.notifications.emitLogin
 import java.util.concurrent.Future
-import java.util.concurrent.TimeUnit
 
 
 class LoginStateService {
-    private var inferenceTask: Future<*>? = null
-
     private var lastWebsiteLoginStatus: String = "OK"
     private var lastInferenceLoginStatus: String = "OK"
+    private var popupLoginMessageOnce: Boolean = false
 
     fun getLastWebsiteLoginStatus(): String {
         return lastWebsiteLoginStatus
@@ -25,8 +25,15 @@ class LoginStateService {
             try {
                 Logger.getInstance("check_login").warn("call")
                 lastWebsiteLoginStatus = checkLogin(force)
+                if (!popupLoginMessageOnce && lastWebsiteLoginStatus.isEmpty()) {
+                    val project = ProjectManager.getInstance().openProjects.firstOrNull() ?: return@submit
+                    emitLogin(project)
+                }
             } catch (e: Exception) {
                 e.message?.let { logError("check_login exception", it) }
+            }
+            finally {
+                popupLoginMessageOnce = true
             }
         }
     }

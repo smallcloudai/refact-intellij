8097b316bbe05cced4b815999031c7544c075163
reymondzzzz <starkov.kirill123@gmail.com>
2023-09-11 16:01:19 +0300

    fix self hosted login with cloud https://github.com/smallcloudai/refact-intellij/issues/30

--- src/main/kotlin/com/smallcloud/refactai/account/LoginUser.kt
+++ src/main/kotlin/com/smallcloud/refactai/account/LoginUser.kt
@@ -12,6 +12,7 @@ import com.smallcloud.refactai.Resources.defaultRecallUrl
 import com.smallcloud.refactai.io.ConnectionStatus
 import com.smallcloud.refactai.io.sendRequest
 import com.smallcloud.refactai.statistic.UsageStatistic
+import com.smallcloud.refactai.struct.DeploymentMode
 import com.smallcloud.refactai.struct.LongthinkFunctionEntry
 import com.smallcloud.refactai.utils.makeGson
 import org.apache.http.client.utils.URIBuilder
@@ -52,103 +53,76 @@ fun logError(scope: String, msg: String, needChange: Boolean = true) {
     }
 }
 
-fun checkLogin(force: Boolean = false): String {
-    val acc = AccountManager
-    val infC = InferenceGlobalContext
-    val isLoggedIn = acc.isLoggedIn
-    if (isLoggedIn && !force) {
-        return ""
-    }
 
-    val streamlinedLoginTicket = acc.ticket
-    var token = acc.apiKey
-    val headers = mutableMapOf(
-            "Content-Type" to "application/json",
-            "Authorization" to ""
-    )
-
-    if (!streamlinedLoginTicket.isNullOrEmpty() && (token.isNullOrEmpty() || force)) {
-        val recallUrl = defaultRecallUrl
-        headers["Authorization"] = "codify-${streamlinedLoginTicket}"
-        try {
-            val result = sendRequest(
-                    recallUrl, "GET", headers, requestProperties = mapOf(
-                    "redirect" to "follow",
-                    "cache" to "no-cache",
-                    "referrer" to "no-referrer"
-            )
-            )
-            val gson = makeGson()
-            val body = gson.fromJson(result.body, JsonObject::class.java)
-            val retcode = body.get("retcode").asString
-            val humanReadableMessage =
-                    if (body.has("human_readable_message")) body.get("human_readable_message").asString else ""
-            if (retcode == "OK") {
-                acc.apiKey = body.get("secret_key").asString
-                acc.ticket = null
-                infC.status = ConnectionStatus.CONNECTED
-                UsageStats.addStatistic(true, UsageStatistic("recall"), recallUrl.toString(), "")
-            } else if (retcode == "FAILED" && humanReadableMessage.contains("rate limit")) {
-                logError("recall", humanReadableMessage, false)
-                return "OK"
-            } else {
-                result.body?.let {
-                    UsageStats.addStatistic(false, UsageStatistic("recall (1)"), recallUrl.toString(), it)
-                    logError("recall", it)
-                }
-                return ""
+private fun tryTicketPass(): String? {
+    val headers = mutableMapOf("Content-Type" to "application/json", "Authorization" to "codify-${AccountManager.ticket}")
+    try {
+        val result = sendRequest(defaultRecallUrl, "GET", headers, requestProperties = mapOf("redirect" to "follow", "cache" to "no-cache", "referrer" to "no-referrer"))
+        val gson = makeGson()
+        val body = gson.fromJson(result.body, JsonObject::class.java)
+        val retcode = body.get("retcode").asString
+        val humanReadableMessage = if (body.has("human_readable_message")) body.get("human_readable_message").asString else ""
+        if (retcode == "OK") {
+            AccountManager.apiKey = body.get("secret_key").asString
+            AccountManager.ticket = null
+            InferenceGlobalContext.status = ConnectionStatus.CONNECTED
+            UsageStats.addStatistic(true, UsageStatistic("recall"), defaultRecallUrl.toString(), "")
+            return null
+        } else if (retcode == "FAILED" && humanReadableMessage.contains("rate limit")) {
+            logError("recall", humanReadableMessage, false)
+            return "OK"
+        } else {
+            result.body?.let {
+                UsageStats.addStatistic(false, UsageStatistic("recall (1)"), defaultRecallUrl.toString(), it)
+                logError("recall", it)
             }
-
-        } catch (e: Exception) {
-            UsageStats.addStatistic(false, UsageStatistic("recall (2)"), recallUrl.toString(), e)
-            e.message?.let { logError("recall", it) }
             return ""
         }
-    }
 
-    token = acc.apiKey
-    if (token.isNullOrEmpty() && InferenceGlobalContext.isCloud) {
+    } catch (e: Exception) {
+        UsageStats.addStatistic(false, UsageStatistic("recall (2)"), defaultRecallUrl.toString(), e)
+        e.message?.let { logError("recall", it) }
         return ""
     }
+}
 
-    val urlBuilder = if (infC.isSelfHosted && infC.inferenceUri != null)
-        URIBuilder(infC.inferenceUri?.resolve(defaultLoginUrlSuffix)) else URIBuilder(defaultLoginUrl)
+private fun buildLoginUrl(): URI {
+    val urlBuilder = if (InferenceGlobalContext.isSelfHosted && InferenceGlobalContext.inferenceUri != null)
+        URIBuilder(InferenceGlobalContext.inferenceUri?.resolve(defaultLoginUrlSuffix)) else URIBuilder(defaultLoginUrl)
 
-    if (infC.developerModeEnabled && infC.stagingVersion.isNotEmpty()) {
-        urlBuilder.addParameter("want_staging_version", infC.stagingVersion)
+    if (InferenceGlobalContext.developerModeEnabled && InferenceGlobalContext.stagingVersion.isNotEmpty()) {
+        urlBuilder.addParameter("want_staging_version", InferenceGlobalContext.stagingVersion)
     }
-    if (infC.isCloud) {
+    if (InferenceGlobalContext.isCloud) {
         urlBuilder.addParameter("plugin_version", "${Resources.client}-${Resources.version}")
     }
-    val url = urlBuilder.build()
+    return urlBuilder.build()
+}
 
-    headers["Authorization"] = "Bearer $token"
+private fun tryLoginWithApiKey(): String {
+    val token = AccountManager.apiKey
+
+    val url = buildLoginUrl()
+    val headers = mutableMapOf("Content-Type" to "application/json", "Authorization" to "Bearer $token")
     try {
-        val result = sendRequest(
-                url, "GET", headers, requestProperties = mapOf(
-                "redirect" to "follow",
-                "cache" to "no-cache",
-                "referrer" to "no-referrer"
-            )
-        )
+        val result = sendRequest(url, "GET", headers, requestProperties = mapOf("redirect" to "follow", "cache" to "no-cache", "referrer" to "no-referrer"))
 
         val gson = makeGson()
         val body = gson.fromJson(result.body, JsonObject::class.java)
         val retcode = body.get("retcode").asString
-        val humanReadableMessage =
-                if (body.has("human_readable_message")) body.get("human_readable_message").asString else ""
+        val humanReadableMessage = if (body.has("human_readable_message")) body.get("human_readable_message").asString else ""
         if (retcode == "OK") {
             if (body.has("account")) {
-                acc.user = body.get("account").asString
+                AccountManager.user = body.get("account").asString
             }
-            acc.ticket = null
+            AccountManager.ticket = null
             if (body.get("inference_url") != null) {
                 if (body.get("inference_url").asString != "DISABLED") {
-                    infC.cloudInferenceUri = URI(body.get("inference_url").asString)
+                    InferenceGlobalContext.cloudInferenceUri = URI(body.get("inference_url").asString)
                 }
             }
             if (body.has("inference")) {
-                acc.activePlan = body.get("inference").asString
+                AccountManager.activePlan = body.get("inference").asString
             }
 
             if (body.has("tooltip_message") && body.get("tooltip_message").asString.isNotEmpty()) {
@@ -174,28 +148,28 @@ fun checkLogin(force: Boolean = false): String {
             }
 
             if (body.has("metering_balance")) {
-                acc.meteringBalance = body.get("metering_balance").asInt
+                AccountManager.meteringBalance = body.get("metering_balance").asInt
             }
 
             if (body.has("chat-v1-style")) {
-                infC.isNewChatStyle = (body.get("chat-v1-style").asInt > 0)
+                InferenceGlobalContext.isNewChatStyle = (body.get("chat-v1-style").asInt > 0)
             }
 
             UsageStats.addStatistic(true, UsageStatistic("login"), url.toString(), "")
-            return if (infC.isCloud) inferenceLogin() else "OK"
+            return if (InferenceGlobalContext.isCloud) inferenceLogin() else "OK"
         } else if (retcode == "FAILED" && humanReadableMessage.contains("rate limit")) {
             logError("login-failed", humanReadableMessage, false)
             UsageStats.addStatistic(false, UsageStatistic("login-failed"), url.toString(), humanReadableMessage)
             return "OK"
         } else if (retcode == "FAILED") {
-            acc.user = null
-            acc.activePlan = null
+            AccountManager.user = null
+            AccountManager.activePlan = null
             logError("login-failed", humanReadableMessage)
             UsageStats.addStatistic(false, UsageStatistic("login-failed"), url.toString(), humanReadableMessage)
             return ""
         } else {
-            acc.user = null
-            acc.activePlan = null
+            AccountManager.user = null
+            AccountManager.activePlan = null
             logError("login-failed", "unrecognized response")
             UsageStats.addStatistic(false, UsageStatistic("login (2)"), url.toString(), "unrecognized response")
             return ""
@@ -205,4 +179,28 @@ fun checkLogin(force: Boolean = false): String {
         UsageStats.addStatistic(false, UsageStatistic("login (3)"), url.toString(), e)
         return ""
     }
+}
+
+fun checkLogin(force: Boolean = false): String {
+    if (AccountManager.isLoggedIn && !force) {
+        return ""
+    }
+
+    when (InferenceGlobalContext.deploymentMode) {
+        DeploymentMode.SELF_HOSTED -> {}
+        DeploymentMode.CLOUD -> {
+            if (!AccountManager.ticket.isNullOrEmpty() &&
+                    (AccountManager.apiKey.isNullOrEmpty() || force)) {
+                val status = tryTicketPass()
+                if (status != null) {
+                    return status
+                }
+            }
+            if (AccountManager.apiKey.isNullOrEmpty()) {
+                return ""
+            }
+        }
+    }
+
+    return tryLoginWithApiKey()
 }

93d7e21e108f06e9c87aa264a8270fe1f80913ab
reymondzzzz <starkov.kirill123@gmail.com>
2023-11-03 15:24:12 +0300

    add tab promoter

---
+++ src/main/kotlin/com/smallcloud/refactai/listeners/AcceptActionPromoter.kt
@@ -0,0 +1,18 @@
+package com.smallcloud.refactai.listeners
+
+import com.intellij.openapi.actionSystem.ActionPromoter
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.actionSystem.DataContext
+import com.intellij.openapi.editor.Editor
+
+class AcceptActionsPromoter : ActionPromoter {
+    private fun getEditor(dataContext: DataContext): Editor? {
+        return CommonDataKeys.EDITOR.getData(dataContext)
+    }
+    override fun promote(actions: MutableList<out AnAction>, context: DataContext): MutableList<AnAction> {
+        if (getEditor(context) == null)
+            return actions.toMutableList()
+        return actions.filterIsInstance<TabPressedAction>().toMutableList()
+    }
+}

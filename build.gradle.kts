0221afbc246be89e4baa8b1a30d9e4bd2af988d7
Kirill Starkov <starkov.kirill123@gmail.com>
2024-05-29 16:37:47 +0800

    rollout build settings

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,8 +34,8 @@ repositories {
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html#intellij-extension-type
 intellij {
 //    version.set("LATEST-EAP-SNAPSHOT")
-    version.set("2024.1.2")
-    type.set("RD") // Target IDE Platform
+    version.set("2022.3.1")
+    type.set("PC") // Target IDE Platform
 
     plugins.set(listOf(
         "Git4Idea",

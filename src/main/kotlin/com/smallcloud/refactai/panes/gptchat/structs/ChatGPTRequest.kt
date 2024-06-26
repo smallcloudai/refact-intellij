28dc0a1aa32f56bbd0da2b26e4507d94ca9ee45c
Kirill Starkov <starkov.kirill123@gmail.com>
2024-06-26 18:05:14 +0800

    remove old chat

--- src/main/kotlin/com/smallcloud/refactai/panes/gptchat/structs/ChatGPTRequest.kt
+++
@@ -1,9 +0,0 @@
-package com.smallcloud.refactai.panes.gptchat.structs
-
-import java.net.URI
-
-data class ChatGPTRequest(
-        val uri: URI,
-        val token: String?,
-        val conversation: List<HistoryEntry>,
-)
--- src/main/kotlin/com/smallcloud/refactai/panes/gptchat/structs/ParsedText.kt
+++
@@ -1,3 +0,0 @@
-package com.smallcloud.refactai.panes.gptchat.structs
-
-data class ParsedText(var rawText: String, var htmlText: String, var isCode: Boolean, var isError: Boolean = false)

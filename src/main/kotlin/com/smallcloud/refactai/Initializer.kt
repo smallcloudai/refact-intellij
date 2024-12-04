dd8a6f6c9693e35e0bd41bd5fad332738b9c2f23
Kirill Starkov <starkov.kirill123@gmail.com>
2024-12-04 18:37:48 +0800

    fix completion

--- src/main/kotlin/com/smallcloud/refactai/Initializer.kt
+++ src/main/kotlin/com/smallcloud/refactai/Initializer.kt
@@ -2,12 +2,16 @@ package com.smallcloud.refactai
 
 import com.intellij.ide.plugins.PluginInstaller
 import com.intellij.openapi.Disposable
+import com.intellij.openapi.actionSystem.IdeActions
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.invokeLater
 import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.keymap.Keymap
+import com.intellij.openapi.keymap.KeymapManagerListener
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
 import com.smallcloud.refactai.io.CloudMessageService
+import com.smallcloud.refactai.listeners.ACTION_ID_
 import com.smallcloud.refactai.listeners.UninstallListener
 import com.smallcloud.refactai.lsp.LSPActiveDocNotifierService
 import com.smallcloud.refactai.lsp.LSPProcessHolder.Companion.initialize
@@ -20,7 +24,6 @@ import com.smallcloud.refactai.settings.settingsStartup
 import com.smallcloud.refactai.utils.isJcefCanStart
 import java.util.concurrent.atomic.AtomicBoolean
 import com.smallcloud.refactai.lsp.LSPProcessHolder.Companion.getInstance as getLSPProcessHolder
-
 
 class Initializer : ProjectActivity, Disposable {
     override suspend fun execute(project: Project) {
@@ -36,6 +39,45 @@ class Initializer : ProjectActivity, Disposable {
             notificationStartup()
             PluginInstaller.addStateListener(UninstallListener())
             UpdateChecker.instance
+
+            ApplicationManager.getApplication()
+                .messageBus
+                .connect(PluginState.instance)
+                .subscribe(KeymapManagerListener.TOPIC, object : KeymapManagerListener {
+                    override fun shortcutsChanged(
+                        keymap: Keymap,
+                        actionIds: MutableCollection<String?>,
+                        fromSettings: Boolean
+                    ) {
+                        if (Thread.currentThread().stackTrace.count { it.className.startsWith("com.smallcloud.refactai.Initializer") } > 1) {
+                            return
+                        }
+                        for (id in actionIds) {
+                            if (!listOf(IdeActions.ACTION_INSERT_INLINE_COMPLETION, ACTION_ID_).contains(id)) {
+                                continue
+                            }
+                            val shortcuts = keymap.getShortcuts(id)
+                            if (id == IdeActions.ACTION_INSERT_INLINE_COMPLETION) {
+                                keymap.removeAllActionShortcuts(ACTION_ID_)
+                                for (shortcut in shortcuts) {
+                                    keymap.addShortcut(
+                                        ACTION_ID_,
+                                        shortcut
+                                    )
+                                }
+                            } else if (id == ACTION_ID_) {
+                                keymap.removeAllActionShortcuts(IdeActions.ACTION_INSERT_INLINE_COMPLETION)
+                                for (shortcut in shortcuts) {
+                                    keymap.addShortcut(
+                                        IdeActions.ACTION_INSERT_INLINE_COMPLETION,
+                                        shortcut
+                                    )
+                                }
+                            }
+                        }
+                    }
+                })
+
             ApplicationManager.getApplication().getService(CloudMessageService::class.java)
             if (!isJcefCanStart()) {
                 emitInfo(RefactAIBundle.message("notifications.chatCanNotStartWarning"), false)
--- src/main/kotlin/com/smallcloud/refactai/listeners/AcceptActionPromoter.kt
+++ src/main/kotlin/com/smallcloud/refactai/listeners/AcceptActionPromoter.kt
@@ -5,18 +5,14 @@ import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.actionSystem.DataContext
 import com.intellij.openapi.editor.Editor
-import com.smallcloud.refactai.modes.ModeProvider
 
 class AcceptActionsPromoter : ActionPromoter {
     private fun getEditor(dataContext: DataContext): Editor? {
         return CommonDataKeys.EDITOR.getData(dataContext)
     }
     override fun promote(actions: MutableList<out AnAction>, context: DataContext): MutableList<AnAction> {
-        val editor = getEditor(context) ?: return actions.toMutableList()
-        val provider = ModeProvider.getOrCreateModeProvider(editor)
-        if (!provider.isInCompletionMode()) {
-            return actions.filterIsInstance<TabPressedAction>().toMutableList()
-        }
-        return actions.toMutableList()
+        if (getEditor(context) == null)
+            return actions.toMutableList()
+        return actions.filterIsInstance<TabPressedAction>().toMutableList()
     }
 }

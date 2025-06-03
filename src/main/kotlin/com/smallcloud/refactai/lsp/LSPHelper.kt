5fa5b9048e9ce6d81134c51fe0689c3b6f0fa2d4
Kirill Starkov <starkov.kirill123@gmail.com>
2025-06-03 14:46:33 +0300

    fix finding roots for rider 2

--- src/main/kotlin/com/smallcloud/refactai/lsp/LSPHelper.kt
+++ src/main/kotlin/com/smallcloud/refactai/lsp/LSPHelper.kt
@@ -3,12 +3,23 @@ package com.smallcloud.refactai.lsp
 import com.google.gson.Gson
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.fileEditor.FileDocumentManager
+import com.intellij.openapi.module.ModuleManager
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.ProjectManager
+import com.intellij.openapi.project.modules
+import com.intellij.openapi.roots.ModuleRootManager
+import com.intellij.openapi.roots.ModuleRootModel
 import com.intellij.openapi.roots.ProjectRootManager
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.platform.backend.workspace.WorkspaceModel
+import com.intellij.platform.backend.workspace.toVirtualFileUrl
+import com.intellij.platform.backend.workspace.workspaceModel
+import com.intellij.util.application
+import com.intellij.workspaceModel.ide.impl.LegacyBridgeJpsEntitySourceFactory
 import com.intellij.workspaceModel.ide.impl.legacyBridge.project.ProjectRootManagerBridge
 import com.smallcloud.refactai.io.ConnectionStatus
 import java.nio.file.Paths
+import kotlin.io.path.Path
 import com.smallcloud.refactai.io.InferenceGlobalContext.Companion.instance as InferenceGlobalContext
 import com.smallcloud.refactai.lsp.LSPProcessHolder.Companion.getInstance as getLSPProcessHolder
 
@@ -29,11 +40,22 @@ fun findRoots(paths: List<String>): List<String> {
 fun lspProjectInitialize(lsp: LSPProcessHolder, project: Project) {
     val projectRootManager = ProjectRootManager.getInstance(project)
     val projectRoots = projectRootManager.contentRoots.map { it.toString() }.ifEmpty {
-        if (projectRootManager is ProjectRootManagerBridge) {
-            return@ifEmpty findRoots(projectRootManager.rootsToWatch.map { it.rootPath })
-        } else {
-            return@ifEmpty emptyList()
+        val listOfFiles: MutableList<String> = mutableListOf(project.basePath.toString())
+        application.runReadAction {
+            project.modules.forEach { module ->
+                val rootManager = ModuleRootManager.getInstance(module)
+                rootManager.fileIndex.iterateContent { vfile ->
+                    if (rootManager.fileIndex.isInContent(vfile) ||
+                        rootManager.fileIndex.isInSourceContent(vfile) ||
+                        rootManager.fileIndex.isInTestSourceContent(vfile)
+                    ) {
+                        listOfFiles.add(vfile.toNioPath().toString())
+                    }
+                    true
+                }
+            }
         }
+        return@ifEmpty findRoots(listOfFiles)
     }.ifEmpty { listOf(project.basePath) }
     val url = lsp.url.resolve("/v1/lsp-initialize")
     val data = Gson().toJson(
